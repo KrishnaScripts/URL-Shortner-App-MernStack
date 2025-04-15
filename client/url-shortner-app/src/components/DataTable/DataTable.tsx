8a8e85e2b950f5c25b6279470963a3ca759d23ed
KrishnaScripts <krishnahpatel2000@gmail.com>
2025-04-15 12:05:06 +0100

    change spelling

--- client/url-shortner-app/src/components/DataTable/DataTable.tsx
+++ client/url-shortner-app/src/components/DataTable/DataTable.tsx
@@ -94,7 +94,7 @@ const Datatable: React.FC<IDataTableProps> = ({ data, updateReloadState }) => {
   
     {/* Mobile Table */}
     <div className="block sm:hidden space-y-4">
-    {!data.length && <p className="text-center text-red-500">No data availablex </p>}
+    {!data.length && <p className="text-center text-red-500">No data available </p>}
 
     {data.map((item, index) => (
       <div key={index} className="bg-white rounded-lg shadow p-4 text-sm">
